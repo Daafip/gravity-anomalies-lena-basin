ddbf9be13b611c91df7ddb997a27ffb0056b0f04
David Haasnoot <d.b.haasnoot@student.tudelft.nl>
2023-10-27 14:18:01 +0200

    update code to include GIA

--- multicore_filtered.py
+++ multicore_filtered.py
@@ -55,31 +55,9 @@ def run_initialisation():
     df_1_0.index = df_index_replace
 
     fname_stoke_coeff2 = f'Data\\Grace\\degree2_stokes_coeff.txt'
-
-    # pd.read_csv didnt work so must read 'manually'
-    reach_end_of_head = False
-    list_lines = []
-    with open(fname_stoke_coeff2) as fin:
-        for line in fin:
-            line = line.strip()
-            if not reach_end_of_head:
-                if line.startswith("Product:"):
-                    reach_end_of_head = True
-            else:
-                line = line.split(' ')
-                line = np.array(line)
-                line = line[line != " "]
-                line = line[line != ""]
-                list_lines.append(line)
-    arr_lines = np.array(list_lines)
-
-    # convert array into df
-    df_stokes_2_3 = pd.DataFrame(arr_lines,
-                                 columns=['MJD begin', "Year fraction begin", "C20", "C20 - C20_mean (1.0E-10)",
-                                          "sig_C20 (1.0E-10)",
-                                          "C30", "C30 - C30_mean (1.0E-10)", "sig_C30 (1.0E-10)",
-                                          'MJD end', "Year fraction end"
-                                          ])
+    col_names = ['MJD begin', "Year fraction begin", "C20", "C20 - C20_mean (1.0E-10)", "sig_C20 (1.0E-10)",
+                 "C30", "C30 - C30_mean (1.0E-10)", "sig_C30 (1.0E-10)", 'MJD end', "Year fraction end"]
+    df_stokes_2_3 = pd.read_csv(fname_stoke_coeff2, skiprows=37, delimiter="\s+", names=col_names)
     # fix date format
     df_stokes_2_3["begin_date"] = df_stokes_2_3.apply(lambda x: MJD_to_ts(x['MJD begin']), axis=1)
     df_stokes_2_3["end_date"] = df_stokes_2_3.apply(lambda x: MJD_to_ts(x['MJD begin']), axis=1)
@@ -88,6 +66,25 @@ def run_initialisation():
     # allign indexes and replace like in C_1_1..
     df_stokes_2_3 = df_stokes_2_3.iloc[:-2]  # remove last two months to make same length
     df_stokes_2_3.index = df_index_replace
+
+    # add trend for Glacial isostatic Adjustment (GIA)
+    fname_gia = f'Data\\GIA\\GIA_stoke_coeff_trend.gz'
+    df_1 = pd.read_csv(fname_gia, compression='gzip', skiprows=1, nrows=6, names=["l", "m", "Clm", "Slm"],
+                       delimiter="\s+")
+    df_2 = pd.read_csv(fname_gia, compression='gzip', skiprows=9, nrows=6, names=["l", "m", "Clm", "Slm"],
+                       delimiter="\s+")
+    df_3 = pd.read_csv(fname_gia, compression='gzip', skiprows=17, names=["l", "m", "Clm", "Slm"], delimiter="\s+")
+
+    df_combined = pd.concat([df_2, df_3])
+    df_combined["l"] = df_combined["l"].astype(int)
+    df_combined["m"] = df_combined["m"].astype(int)
+
+    GIA_C = np.zeros((l + 1, m + 1))
+    GIA_S = np.zeros((l + 1, m + 1))
+    for index, row in df_combined.iterrows():
+        if row.m <= m and row.l <= l:
+            GIA_C[int(row.m), int(row.l)] = row["Clm"]
+            GIA_S[int(row.m), int(row.l)] = row["Slm"]
 
     #############################################################
     # load in all coefficient
@@ -109,57 +106,54 @@ def run_initialisation():
         S_i[1, 1] = df_1_1.loc[new_time, "Slm"]
         C_i[2, 0] = df_stokes_2_3.loc[new_time, "C20"]
         C_30 = df_stokes_2_3.loc[new_time, "C30"]
-        if C_30 == "NaN":
+        # print(C_30)
+        if np.isnan(C_30):
             pass
         else:
             C_i[3, 0] = C_30
 
+        # add glacia isostatic adjustment
+        C_i += GIA_C
+        S_i += GIA_S
+
         C.append(C_i), S.append(S_i), times.append(time_i), grace_names.append(
             f'{time_i.year}-{time_i.month}-{time_i.day}'), plot_times.append(mid_date_ts)
     #############################################################
-    # get differences for each pair
+    # calculate means of coefficients
+    C = np.array(C)
+    S = np.array(S)
+    C_mean = C.sum(axis=0) / len(C)
+    S_mean = S.sum(axis=0) / len(S)
+    # remove mean coefficients
     dc1_store = []
-    dc2_store = []
     ds1_store = []
-    ds2_store = []
     for i, c in enumerate(C):
-        if i == 0:
-            pass  # first has no prev
-        else:
-            dc1 = C[i] - ((C[i] + C[i - 1]) / 2)
-            dc2 = C[i - 1] - ((C[i - 1] + C[i]) / 2)
-            ds1 = S[i] - ((S[i] + S[i - 1]) / 2)
-            ds2 = S[i - 1] - ((S[i - 1] + S[i]) / 2)
-            dc1_store.append(dc1), dc2_store.append(dc2)
-            ds1_store.append(ds1), ds2_store.append(ds2)
+        dc1 = C[i] - C_mean
+        ds1 = S[i] - S_mean
+        dc1_store.append(dc1)
+        ds1_store.append(ds1)
 
     ##############################################################
     rho_av__rho_w = 5.5  # aprox
     # convert to dh
     dh_c1_store = []
-    dh_c2_store = []
     dh_s1_store = []
-    dh_s2_store = []
 
     for k, dc1 in enumerate(dc1_store):
         C_dhw_i_1 = np.zeros((l + 1, l + 1))
         S_dhw_i_1 = np.zeros((l + 1, l + 1))
-        C_dhw_i_2 = np.zeros((l + 1, l + 1))
-        S_dhw_i_2 = np.zeros((l + 1, l + 1))
         for i in range(l + 1):
             for j in range(i + 1):
                 multiplication_factor = (R * (2 * i + 1) * rho_av__rho_w) / (3 * (1 + love_numbers_kl[i]))
                 C_dhw_i_1[i, j] = (dc1_store[k][i, j] * multiplication_factor)
                 S_dhw_i_1[i, j] = (ds1_store[k][i, j] * multiplication_factor)
-                C_dhw_i_2[i, j] = (dc2_store[k][i, j] * multiplication_factor)
-                S_dhw_i_2[i, j] = (ds2_store[k][i, j] * multiplication_factor)
-        dh_c1_store.append(C_dhw_i_1), dh_c2_store.append(C_dhw_i_2)
-        dh_s1_store.append(S_dhw_i_1), dh_s2_store.append(S_dhw_i_2)
+        dh_c1_store.append(C_dhw_i_1)
+        dh_s1_store.append(S_dhw_i_1)
 
     _lambda = np.pi / 180 * np.arange(270, 330, 1) - np.pi  # 90 - 150 # deg lon
     theta = np.pi - np.pi / 180 * np.arange(180 - 40, 180 - 10, 1)  # 80 - 50 deg lat
 
-    return _lambda, theta, dh_c1_store, dh_c2_store, dh_s1_store, dh_s2_store, plot_times
+    return _lambda, theta, dh_c1_store, dh_s1_store, plot_times
 
 def plm(theta, degree):
     p_lm = np.zeros((degree + 1, degree + 1))
@@ -195,9 +189,8 @@ def plm(theta, degree):
 
 
 def run_loop(input):
-    _lambda , theta, dh_c1_store, dh_c2_store, dh_s1_store, dh_s2_store = input
+    _lambda , theta, dh_c1_store, dh_s1_store = input
     ewh_i_1 = np.zeros((len(theta), len(_lambda)))
-    ewh_i_2 = np.zeros((len(theta), len(_lambda)))
     m, l = 60, 60
     for i in range(len(theta)):  # loop over all thetas
         # print(f'{i=} (out of {len(theta)})',end='\r')
@@ -209,11 +202,9 @@ def run_loop(input):
                     cos_t_lambda = np.cos(t * _lambda[j])  # non-negative orders
                     # compute here equivalent water heights
                     ewh_i_1[i, j] = ewh_i_1[i, j] + (dh_s1_store[k, t] * P_lm[k, t] * sin_t_lambda)
-                    ewh_i_2[i, j] = ewh_i_2[i, j] + (dh_s2_store[k, t] * P_lm[k, t] * sin_t_lambda)
                     ewh_i_1[i, j] = ewh_i_1[i, j] + (dh_c1_store[k, t] * P_lm[k, t] * cos_t_lambda)
-                    ewh_i_2[i, j] = ewh_i_2[i, j] + (dh_c2_store[k, t] * P_lm[k, t] * cos_t_lambda)
 
-    return ewh_i_1 - ewh_i_2
+    return ewh_i_1
 
 
 
@@ -277,17 +268,17 @@ if __name__ == "__main__":
     #### last run  240. second - ~4 min, n=80
     print("start")
     # first way, using multiprocessing
-    _lambda, theta, dh_c1_store, dh_c2_store, dh_s1_store, dh_s2_store, times = run_initialisation()
+    _lambda, theta, dh_c1_store, dh_s1_store, times = run_initialisation()
     debug = False
     if debug:
-        n = 10
-        args = [[_lambda, theta, dh_c1_store[i], dh_c2_store[i], dh_s1_store[i], dh_s2_store[i]]
-                for i in range(len(dh_s2_store[:n]))]
-        times = times[:(n + 1)]  # debug
+        n = 1
+        args = [[_lambda, theta, dh_c1_store[i], dh_s1_store[i]]
+                for i in range(len(dh_s1_store[:n]))]
+        times = times[:(n)]  # debug
         fname = "Data\\anomally_waterhead_filtered_test_multicore.nc" # debug
     else:
-        args = [[_lambda, theta, dh_c1_store[i], dh_c2_store[i], dh_s1_store[i], dh_s2_store[i]]
-                for i in range(len(dh_s2_store))]
+        args = [[_lambda, theta, dh_c1_store[i], dh_s1_store[i]]
+                for i in range(len(dh_s1_store))]
         fname = "Data\\anomally_waterhead_filtered_multicore.nc"
     print("initialisation done")
     start_time = time.time()
@@ -300,51 +291,7 @@ if __name__ == "__main__":
 
     ds = xr.DataArray(store_ewh, dims=("time", "lat", "lon"), coords={"lon": _lambda / np.pi * 180,
                                                                       "lat": 90 - theta/np.pi*180,
-                                                                      "time": times[1:]}, name="dh(m)")
+                                                                      "time": times}, name="dh(m)")
 
 
     ds.to_netcdf(fname)
-
-# # store each month seperately data differences
-
-### shouldn't be the approach as we need the deltas between the wanted periods
-# if __name__ == "__main__":
-#     #### last full run  514.2109310626984 second - ~10 min
-#     #### last run  240. second - ~4 min, n=80
-#     print("start")
-#     # first way, using multiprocessing
-#     _lambda, theta, dh_c1_store, dh_c2_store, dh_s1_store, dh_s2_store, times = run_initialisation()
-#     debug = False
-#     if debug:
-#         n = 3
-#         args = [[_lambda, theta, dh_c1_store[i], dh_c2_store[i], dh_s1_store[i], dh_s2_store[i], True]
-#                 for i in range(len(dh_s2_store[:n]))]
-#         times = times[:(n + 1)]  # debug
-#         fname1 = "Data\\anomally_waterhead_filtered_test_multicore_seperate1.nc" # debug
-#         fname2 = "Data\\anomally_waterhead_filtered_test_multicore_seperate2.nc"  # debug
-#     else:
-#         args = [[_lambda, theta, dh_c1_store[i], dh_c2_store[i], dh_s1_store[i], dh_s2_store[i], True]
-#                 for i in range(len(dh_s2_store))]
-#         fname1 = "Data\\anomally_waterhead_filtered_multicore_seperate1.nc"
-#         fname2 = "Data\\anomally_waterhead_filtered_multicore_seperate2.nc"
-#     print("initialisation done")
-#     start_time = time.time()
-#     print(datetime.datetime.now())
-#     with Pool() as pool:
-#         store_ewh = pool.map(run_loop, args)
-#
-#     store_ewh = np.array(store_ewh)
-#     store_ewh1 = store_ewh[:,0]
-#     store_ewh2 = store_ewh[:,1]
-#     finish_time = time.time()
-#     print("Program finished in {} seconds - using multiprocessing".format(finish_time - start_time))
-#
-#     ds1 = xr.DataArray(store_ewh1, dims=("time", "lat", "lon"), coords={"lon": _lambda / np.pi * 180,
-#                                                                       "lat": 90 - theta/np.pi*180,
-#                                                                       "time": times[1:]}, name="dh(m)")
-#     ds2 = xr.DataArray(store_ewh2, dims=("time", "lat", "lon"), coords={"lon": _lambda / np.pi * 180,
-#                                                                       "lat": 90 - theta/np.pi*180,
-#                                                                       "time": times[1:]}, name="dh(m)")
-#
-#     ds1.to_netcdf(fname1)
-#     ds2.to_netcdf(fname2)
--- multicore_raw.py
+++ multicore_raw.py
@@ -21,7 +21,7 @@ import xarray as xr
 from calc_geoid_change_alt import readstrokescoefficients, plm
 from multiprocessing import Pool
 
-def run_initialisation():
+def run_initialisation_raw():
     grace_files = glob.glob('Data\\Grace\*.gfc')
     love_numbers_kl = np.loadtxt('Data\\loadLoveNumbers_60.txt')[:,1]
     l = 60
@@ -55,31 +55,9 @@ def run_initialisation():
     df_1_0.index = df_index_replace
 
     fname_stoke_coeff2 = f'Data\\Grace\\degree2_stokes_coeff.txt'
-
-    # pd.read_csv didnt work so must read 'manually'
-    reach_end_of_head = False
-    list_lines = []
-    with open(fname_stoke_coeff2) as fin:
-        for line in fin:
-            line = line.strip()
-            if not reach_end_of_head:
-                if line.startswith("Product:"):
-                    reach_end_of_head = True
-            else:
-                line = line.split(' ')
-                line = np.array(line)
-                line = line[line != " "]
-                line = line[line != ""]
-                list_lines.append(line)
-    arr_lines = np.array(list_lines)
-
-    # convert array into df
-    df_stokes_2_3 = pd.DataFrame(arr_lines,
-                                 columns=['MJD begin', "Year fraction begin", "C20", "C20 - C20_mean (1.0E-10)",
-                                          "sig_C20 (1.0E-10)",
-                                          "C30", "C30 - C30_mean (1.0E-10)", "sig_C30 (1.0E-10)",
-                                          'MJD end', "Year fraction end"
-                                          ])
+    col_names = ['MJD begin', "Year fraction begin", "C20", "C20 - C20_mean (1.0E-10)", "sig_C20 (1.0E-10)",
+                 "C30", "C30 - C30_mean (1.0E-10)", "sig_C30 (1.0E-10)", 'MJD end', "Year fraction end"]
+    df_stokes_2_3 = pd.read_csv(fname_stoke_coeff2, skiprows=37, delimiter="\s+", names=col_names)
     # fix date format
     df_stokes_2_3["begin_date"] = df_stokes_2_3.apply(lambda x: MJD_to_ts(x['MJD begin']), axis=1)
     df_stokes_2_3["end_date"] = df_stokes_2_3.apply(lambda x: MJD_to_ts(x['MJD begin']), axis=1)
@@ -89,12 +67,31 @@ def run_initialisation():
     df_stokes_2_3 = df_stokes_2_3.iloc[:-2]  # remove last two months to make same length
     df_stokes_2_3.index = df_index_replace
 
+    # add trend for Glacial isostatic Adjustment (GIA)
+    fname_gia = f'Data\\GIA\\GIA_stoke_coeff_trend.gz'
+    df_1 = pd.read_csv(fname_gia, compression='gzip', skiprows=1, nrows=6, names=["l", "m", "Clm", "Slm"],
+                       delimiter="\s+")
+    df_2 = pd.read_csv(fname_gia, compression='gzip', skiprows=9, nrows=6, names=["l", "m", "Clm", "Slm"],
+                       delimiter="\s+")
+    df_3 = pd.read_csv(fname_gia, compression='gzip', skiprows=17, names=["l", "m", "Clm", "Slm"], delimiter="\s+")
+
+    df_combined = pd.concat([df_2, df_3])
+    df_combined["l"] = df_combined["l"].astype(int)
+    df_combined["m"] = df_combined["m"].astype(int)
+
+    GIA_C = np.zeros((l + 1, m + 1))
+    GIA_S = np.zeros((l + 1, m + 1))
+    for index, row in df_combined.iterrows():
+        if row.m <= m and row.l <= l:
+            GIA_C[int(row.m), int(row.l)] = row["Clm"]
+            GIA_S[int(row.m), int(row.l)] = row["Slm"]
+
+    #############################################################
     # Names and dates needed, obtained from file name
     grace_names = [file[-11:-4] for file in grace_files]
     times = [pd.Timestamp(grace_names_i) for grace_names_i in grace_names]
-    #############################################################
     # load in all coefficient
-    C, S = [], []
+    C, S, = [], []
     for i, file in enumerate(grace_files):
         C_i, S_i, R, GM = readstrokescoefficients(file)
 
@@ -111,58 +108,52 @@ def run_initialisation():
         S_i[1, 1] = df_1_1.loc[new_time, "Slm"]
         C_i[2, 0] = df_stokes_2_3.loc[new_time, "C20"]
         C_30 = df_stokes_2_3.loc[new_time, "C30"]
-        if C_30 == "NaN":
+        if np.isnan(C_30):
             pass
         else:
             C_i[3, 0] = C_30
 
+        # add glacia isostatic adjustment
+        C_i += GIA_C
+        S_i += GIA_S
+
         C.append(C_i), S.append(S_i)
     #############################################################
-    # get differences for each pair
+    # calculate means of coefficients
+    C = np.array(C)
+    S = np.array(S)
+    C_mean = C.sum(axis=0) / len(C)
+    S_mean = S.sum(axis=0) / len(S)
+    # remove mean coefficients
     dc1_store = []
-    dc2_store = []
     ds1_store = []
-    ds2_store = []
     for i, c in enumerate(C):
-        if i == 0:
-            pass  # first has no prev
-        else:
-            dc1 = C[i] - ((C[i] + C[i - 1]) / 2)
-            dc2 = C[i - 1] - ((C[i - 1] + C[i]) / 2)
-            ds1 = S[i] - ((S[i] + S[i - 1]) / 2)
-            ds2 = S[i - 1] - ((S[i - 1] + S[i]) / 2)
-            dc1_store.append(dc1), dc2_store.append(dc2)
-            ds1_store.append(ds1), ds2_store.append(ds2)
+        dc1 = C[i] - C_mean
+        ds1 = S[i] - S_mean
+        dc1_store.append(dc1)
+        ds1_store.append(ds1)
 
     ##############################################################
     rho_av__rho_w = 5.5  # aprox
     # convert to dh
     dh_c1_store = []
-    dh_c2_store = []
     dh_s1_store = []
-    dh_s2_store = []
 
     for k, dc1 in enumerate(dc1_store):
         C_dhw_i_1 = np.zeros((l + 1, l + 1))
         S_dhw_i_1 = np.zeros((l + 1, l + 1))
-        C_dhw_i_2 = np.zeros((l + 1, l + 1))
-        S_dhw_i_2 = np.zeros((l + 1, l + 1))
         for i in range(l + 1):
             for j in range(i + 1):
                 multiplication_factor = (R * (2 * i + 1) * rho_av__rho_w) / (3 * (1 + love_numbers_kl[i]))
                 C_dhw_i_1[i, j] = (dc1_store[k][i, j] * multiplication_factor)
                 S_dhw_i_1[i, j] = (ds1_store[k][i, j] * multiplication_factor)
-                C_dhw_i_2[i, j] = (dc2_store[k][i, j] * multiplication_factor)
-                S_dhw_i_2[i, j] = (ds2_store[k][i, j] * multiplication_factor)
-        dh_c1_store.append(C_dhw_i_1), dh_c2_store.append(C_dhw_i_2)
-        dh_s1_store.append(S_dhw_i_1), dh_s2_store.append(S_dhw_i_2)
+        dh_c1_store.append(C_dhw_i_1)
+        dh_s1_store.append(S_dhw_i_1)
 
     _lambda = np.pi / 180 * np.arange(270, 330, 1) - np.pi  # 90 - 150 # deg lon
     theta = np.pi - np.pi / 180 * np.arange(180 - 40, 180 - 10, 1)  # 80 - 50 deg lat
 
-
-
-    return _lambda, theta, dh_c1_store, dh_c2_store, dh_s1_store, dh_s2_store, times
+    return _lambda, theta, dh_c1_store, dh_s1_store, times
 
 def plm(theta, degree):
     p_lm = np.zeros((degree + 1, degree + 1))
@@ -198,9 +189,8 @@ def plm(theta, degree):
 
 
 def run_loop(input):
-    _lambda , theta, dh_c1_store, dh_c2_store, dh_s1_store, dh_s2_store = input
+    _lambda , theta, dh_c1_store, dh_s1_store = input
     ewh_i_1 = np.zeros((len(theta), len(_lambda)))
-    ewh_i_2 = np.zeros((len(theta), len(_lambda)))
     m, l = 60, 60
     for i in range(len(theta)):  # loop over all thetas
         # print(f'{i=} (out of {len(theta)})',end='\r')
@@ -212,41 +202,9 @@ def run_loop(input):
                     cos_t_lambda = np.cos(t * _lambda[j])  # non-negative orders
                     # compute here equivalent water heights
                     ewh_i_1[i, j] = ewh_i_1[i, j] + (dh_s1_store[k, t] * P_lm[k, t] * sin_t_lambda)
-                    ewh_i_2[i, j] = ewh_i_2[i, j] + (dh_s2_store[k, t] * P_lm[k, t] * sin_t_lambda)
                     ewh_i_1[i, j] = ewh_i_1[i, j] + (dh_c1_store[k, t] * P_lm[k, t] * cos_t_lambda)
-                    ewh_i_2[i, j] = ewh_i_2[i, j] + (dh_c2_store[k, t] * P_lm[k, t] * cos_t_lambda)
 
-    return ewh_i_1 - ewh_i_2
-
-
-def readstrokescoefficients(filename):
-    with open(filename) as f:
-        reach_end_of_head = 0
-        for line in f:
-            line = line.strip()
-            if reach_end_of_head == 0:
-                if line.startswith("earth_gravity_constant"):
-                    GM = float(line[line.index(" ") + 1:])
-                elif "radius" in line:
-                    line = re.sub(' +', ' ', line)
-                    R = float(line.split(" ")[1])
-                elif line.startswith("max_degree"):
-                    line = re.sub(' +', ' ', line)
-                    max_degree = int(line.split(" ")[1])
-                    C = np.zeros((max_degree + 1, max_degree + 1))
-                    S = np.zeros((max_degree + 1, max_degree + 1))
-                else:
-                    if line.startswith("end_of_head"):
-                        reach_end_of_head = 1
-            else:
-                line = re.sub(' +', ' ', line)
-                line = line.split()
-                L = int(line[1])
-                M = int(line[2])
-                C[L, M] = float(line[3])
-                S[L, M] = float(line[4])
-    return C, S, R, GM
-
+    return ewh_i_1
 
 def MJD_to_ts(mjd):
     ## thanks to https://stackoverflow.com/questions/72597699/how-do-i-convert-mjdjulian-date-to-utc-using-astropy
@@ -261,23 +219,23 @@ def MJD_to_ts(mjd):
     return str
 
 
-
+# for monthly data differences
 if __name__ == "__main__":
     #### last full run  514.2109310626984 second - ~10 min
     #### last run  240. second - ~4 min, n=80
     print("start")
     # first way, using multiprocessing
-    _lambda, theta, dh_c1_store, dh_c2_store, dh_s1_store, dh_s2_store, times = run_initialisation()
+    _lambda, theta, dh_c1_store, dh_s1_store, times = run_initialisation_raw()
     debug = False
     if debug:
-        n = 10
-        args = [[_lambda, theta, dh_c1_store[i], dh_c2_store[i], dh_s1_store[i], dh_s2_store[i]]
-                for i in range(len(dh_s2_store[:n]))]
-        times = times[:(n + 1)]  # debug
-        fname = "Data\\anomally_waterhead_raw_test_multicore2.nc" # debug
+        n = 5
+        args = [[_lambda, theta, dh_c1_store[i], dh_s1_store[i]]
+                for i in range(len(dh_s1_store[:n]))]
+        times = times[:(n)]  # debug
+        fname = "Data\\anomally_waterhead_raw_test_multicore.nc" # debug
     else:
-        args = [[_lambda, theta, dh_c1_store[i], dh_c2_store[i], dh_s1_store[i], dh_s2_store[i]]
-                for i in range(len(dh_s2_store))]
+        args = [[_lambda, theta, dh_c1_store[i], dh_s1_store[i]]
+                for i in range(len(dh_s1_store))]
         fname = "Data\\anomally_waterhead_raw_multicore.nc"
     print("initialisation done")
     start_time = time.time()
@@ -286,11 +244,11 @@ if __name__ == "__main__":
         store_ewh = pool.map(run_loop, args)
 
     finish_time = time.time()
-    print("Program finished in {} seconds - using multiprocessing".format(finish_time - start_time))
+    print("Program finished in {} min - using multiprocessing".format((finish_time - start_time)/60))
 
     ds = xr.DataArray(store_ewh, dims=("time", "lat", "lon"), coords={"lon": _lambda / np.pi * 180,
                                                                       "lat": 90 - theta/np.pi*180,
-                                                                      "time": times[1:]}, name="dh(m)")
+                                                                      "time": times}, name="dh(m)")
 
 
     ds.to_netcdf(fname)
